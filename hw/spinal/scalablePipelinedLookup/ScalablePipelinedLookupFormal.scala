04a916f144ded8e2c885e96d892f7d80d12b9f55
Marek Pikuła <marek@serenitycode.dev>
2023-02-13 21:49:01 +0100

    Import base SpinalHDL project
    Signed-off-by: Marek Pikuła <marek@serenitycode.dev>

---
+++ hw/spinal/scalablePipelinedLookup/ScalablePipelinedLookupFormal.scala
@@ -0,0 +1,24 @@
+import scalablePipelinedLookup.ScalablePipelinedLookup
+
+import spinal.core._
+import spinal.core.formal._
+
+// You need SymbiYosys to be installed.
+// See https://spinalhdl.github.io/SpinalDoc-RTD/master/SpinalHDL/Formal%20verification/index.html#installing-requirements
+object ScalablePipelinedLookupFormal extends App {
+  FormalConfig
+    .withBMC(10)
+    .doVerify(new Component {
+      val dut = FormalDut(ScalablePipelinedLookup())
+
+      // Ensure the formal test start with a reset
+      assumeInitial(clockDomain.isResetActive)
+
+      // Provide some stimulus
+      anyseq(dut.io.cond0)
+      anyseq(dut.io.cond1)
+
+      // Check the state initial value and increment
+      assert(dut.io.state === past(dut.io.state + U(dut.io.cond0)).init(0))
+    })
+}
