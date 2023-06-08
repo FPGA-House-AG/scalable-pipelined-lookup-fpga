ca659c961954d66ebcc5b6a1ccb5c347fa3c46c1
Leon Woestenberg <leon@sidebranch.com>
2023-06-08 13:41:54 +0200

    TB: WIP on supporting frug.ipp. Two cycle latency.

--- sbp_lookup_tb.cc
+++ sbp_lookup_tb.cc
@@ -7,6 +7,7 @@
 
 #include <limits.h>
 #include <stdlib.h>
+#include <assert.h>
 
 #include "Vsbp_lookup.h"
 
@@ -17,13 +18,48 @@
 
 #define VCD 1
 
-#define LATENCY (33)
+#define LATENCY (65)
 uint32_t ip_addr_i[LATENCY];
 uint32_t ip_addr2_i[LATENCY];
 int ip_addr_index = 0;
 
+int read_prefix(FILE *fp, uint32_t *prefix_p, int *prefix_len_p)
+{
+  int result = -1;
+  int numbers[4];
+  int prefix_len = 0;
+  int rc;
+  uint32_t addr = 0;
+  if ((prefix_p == NULL) || (prefix_len_p == NULL)) goto end;
+  rc = fscanf(fp, "%d.%d.%d.%d/%d\n", &numbers[0], &numbers[1], &numbers[2], &numbers[3], &prefix_len);
+  if (rc != 5) {
+    perror("fscanf()");
+    goto end;
+  }
+  for (int i = 0; i < 4; i++) {
+    if ((numbers[i] < 0) || (numbers[i] >= 255)) goto end;
+    addr <<= 8;
+    addr |= numbers[i];
+  }
+  //printf("%d.%d.%d.%d/%d ", numbers[0], numbers[1], numbers[2], numbers[3], prefix_len);
+  //printf("0x%08x\n", addr);
+  *prefix_p = addr; 
+  *prefix_len_p = prefix_len;
+  result = 0;
+end:
+  return result;
+}
+
 int main(int argc, char **argv)
 {
+  FILE *fp;
+  fp = fopen("../scalable-pipelined-lookup-c/frug.ipp", "r");
+  if (fp == NULL) return -1;
+
+  uint32_t prefix;
+  int prefix_len;
+  int rc;
+
   // Initialize Verilators variables
   Verilated::commandArgs(argc, argv);
 
@@ -52,14 +88,14 @@ int main(int argc, char **argv)
 
   tb->clk = 0;
   tb->rst = 0;
-  tb->eval();
   tb->ip_addr_i = 0x00000000u;
+  tb->eval();
   // Tick the clock until we are done
   //	while(!Verilated::gotFinish()) {
   //for (int t = 0; t < (n * x_range); t++)
   int cycles = 0;
 
-  while (cycles < 64)
+  while (cycles < 64*2)
   {
     // set inputs
 
@@ -73,6 +109,7 @@ int main(int argc, char **argv)
     tb->upd_childs_location_i = 0;
     tb->upd_childs_lr_i = 0;
     tb->upd_i = 0;
+#if 0
     /* update cycle */
     if (cycles == 1) { 
       tb->upd_ip_addr_i = 0x327b23c0u;
@@ -86,18 +123,37 @@ int main(int argc, char **argv)
       tb->upd_childs_lr_i = 0;
       tb->upd_i = 1;
     }
-#if 0
-    if (cycles == 3)
+#endif
+#if 1
+    if (cycles == 3) {
+      tb->lookup_i = 1;
       tb->ip_addr_i = ip_addr_i[ip_addr_index % LATENCY] = 0x7545e140u;
-    else
+      tb->ip_addr2_i = ip_addr2_i[ip_addr_index % LATENCY] = 0x7545e140u;
+    }
 #endif    
-    if (cycles == 6)
+#if 0
+    else if (cycles == 6) {
+      tb->lookup_i = 1;
       tb->ip_addr_i  = ip_addr_i [ip_addr_index % LATENCY] = 0x327b23f0u;
       tb->ip_addr2_i = ip_addr2_i[ip_addr_index % LATENCY] = 0x62555800u;
-#if 0
-    else if (cycles > 4)
-      tb->ip_addr_i = ip_addr_i[ip_addr_index % LATENCY] = (rand() % UINT32_MAX);
+    }
 #endif
+#if 0
+    else if (cycles > 4) {
+      tb->lookup_i = 1;
+      tb->ip_addr_i = ip_addr_i[ip_addr_index % LATENCY] = (rand() % UINT32_MAX);
+    }
+#endif
+#if 0
+    else if (cycles > 4) {
+      rc = read_prefix(fp, &prefix, &prefix_len);
+      tb->lookup_i = 1;
+      tb->ip_addr_i = ip_addr_i[ip_addr_index % LATENCY] = prefix;
+    }
+#endif
+    else {
+      tb->lookup_i = 0;
+    }
     // falling edge
     tb->clk = 0;
     tb->eval();
@@ -119,5 +175,8 @@ int main(int argc, char **argv)
   }
   tfp->close();
   printf("%s: %s\n", argv[0], test_result?"FAILED":"PASSED");
+
+  if (fp != NULL ) fclose(fp);
+
   exit(test_result);
 }
